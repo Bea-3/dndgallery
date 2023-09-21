3957bfc18b65ea2abc6c997db6eec5ee1b2008e9
beatrice <beatrice.pepple@gmail.com>
2023-09-21 16:51:14 +0100

    implemented search filter method and installed firebase for authentication

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <div className="welcome">
+            <h1>Welcome to DND Gallery </h1>
+
+            <p>Login Here to Add Images to your personalized gallery</p>
+            </div>
+            
+
+            <div className="grid-wrapper">
+                
+                    <div className="pics"></div>
+                    <div className="pics"></div>
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
